83c166f538555fbe65283b802a374e99943bb3b2
Tyler <twenerstrom@gmail.com>
2022-08-10 17:20:18 -0600

    prompt questions created

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "project name",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "Description",
+      message: "Describe your project in as much detail as you like.",
+    },
+    {
+      type: "input",
+      name: "Table of Contents",
+      message: "Describe your project in as much detail as you like.",
+    },
+    {
+      type: "input",
+      name: "Installation",
+      message: "Provide installation instructions.",
+    },
+    {
+      type: "input",
+      name: "Usage",
+      message: "Explain the usage information.",
+    },
+    {
+      type: "input",
+      name: "Contributing",
+      message: "What are the contribution guidelines?",
+    },
+    {
+      type: "input",
+      name: "Tests",
+      message: "Provide test instructions.",
+    },
+  ])
+  // TODO: Create a function to write README file
+  .then((answers) => {
+    const readme = generateREADME(answers);
 
-// TODO: Create a function to write README file
+    fs.writeFile("README.md", readme, (err) =>
+      err ? console.log(err) : console.log("README has been created!")
+    );
+  });
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
