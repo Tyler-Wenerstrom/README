a19f8c5927d193ce2993c08396cdd19df184af86
Tyler <twenerstrom@gmail.com>
2022-08-12 13:23:49 -0600

    fixed files

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "") {
+    return `![GitHub License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  } else "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "") {
+    return ``;
+  } else "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,6 +21,8 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
+  ${renderLicenseBadge(data.license)}
+
   # ${data.title}
 
   ## Table of Contents
@@ -21,17 +31,31 @@ function generateMarkdown(data) {
   -[Usage information](#Usage)
   -[Contribution guidelines](#Contributing)
   -[Test instructions](#Tests)
+  -[License](#license)
 
   ## Description
   ${data.description}
+
   ## Installation
   ${data.installation}
+
   ## Usage
   ${data.usage}
+  
   ## Contributing
   ${data.contributing}
+
   ## Tests
   ${data.tests}
+
+  ## Questions
+
+  [${data.GitHub}](https://www.github.com/${data.GitHub})
+
+
+  ${data.email}
+  
+  
 `;
 }
 
--- index.js
+++ index.js
@@ -1,43 +1,60 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const markDown = require("./generateMarkdown");
+const generatemarkdown = require("./generateMarkdown");
+
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: "input",
-    name: "Title",
+    name: "title",
     message: "What is the name of your project?",
   },
   {
     type: "input",
-    name: "Description",
+    name: "description",
     message: "Describe your project in as much detail as you like.",
   },
+  // {
+  //   type: "input",
+  //   name: "Table of Contents",
+  //   message: "Describe your project in as much detail as you like.",
+  // },
   {
     type: "input",
-    name: "Table of Contents",
-    message: "Describe your project in as much detail as you like.",
-  },
-  {
-    type: "input",
-    name: "Installation",
+    name: "installation",
     message: "Provide installation instructions.",
   },
   {
     type: "input",
-    name: "Usage",
+    name: "usage",
     message: "Explain the usage information.",
   },
   {
     type: "input",
-    name: "Contributing",
+    name: "contributing",
     message: "What are the contribution guidelines?",
   },
   {
     type: "input",
-    name: "Tests",
+    name: "tests",
     message: "Provide test instructions.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license.",
+    choices: ["MIT", "BSL", "Apache", "Unlicense"],
+  },
+  {
+    type: "input",
+    name: "GitHub",
+    message: "Enter your GitHub user name",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "enter your email address.",
   },
 ];
 // TODO: Create a function to write README file
@@ -54,7 +71,10 @@ function writeToFile(fileName, data) {
 function init() {
   return inquirer.prompt(questions).then((answers) => {
     console.log(answers);
-    return markDown;
+    const mrkdwn = generatemarkdown(answers);
+    fs.writeFile("README.md", mrkdwn, (err) =>
+      err ? console.group(err) : console.log("README has been created!")
+    );
   });
 }
 
